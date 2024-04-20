fb6211b13783e7031a6c93a0f704395bf05e03be
YKh6729 <155624221+YKh6729@users.noreply.github.com>
2024-04-20 13:30:23 +0400

    Yura/lecture test (#41)
    * test commit
    
    * delete password from update
    
    * fix delete password

--- vallidations/usersValidation.js
+++ vallidations/usersValidation.js
@@ -28,7 +28,6 @@ const createUserSchema = {
 
 const updateUserSchema = {
   type: "object",
-  required: ["password"],
   properties: {
     username: {
       type: "string",
@@ -43,12 +42,6 @@ const updateUserSchema = {
       type: "number",
       minimum: 1,
       maximum: 4,
-    },
-    password: {
-      type: "string",
-      pattern: "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
-      errorMessage:
-        "Password should be min 8 characters, must contain one uppercase English letter, must contain minimum one lowercase English letter, must contain at least one special character (#?!@$%^&*-), must contain at least one digit",
     },
   },
   additionalProperties: false,

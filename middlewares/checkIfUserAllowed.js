ae8d866405b25ba71f9718b77a1be474f45cfce6
P. Frankyan <petofrankyan@gmail.com>
2024-04-27 15:46:17 +0400

    fixed_checkUserAllowed_middlewares_for_updatePassword

--- middlewares/checkIfUserAllowed.js
+++ middlewares/checkIfUserAllowed.js
@@ -12,23 +12,6 @@ const checkIfUserAllowed = (resource) => async (req, res, next) => {
       .withGraphFetched("role");
 
     const userRole = user.role.role_name.toUpperCase();
-    if (resource === RESOURCE.USER) {
-      if (
-        (req.method === HTTP_METHOD.DELETE || req.method === HTTP_METHOD.PUT) &&
-        (userRole === ROLE_NAME.ADMIN || userRole === ROLE_NAME.SUPERADMIN)
-      ) {
-        next();
-        return;
-      } else if (
-        (req.method === HTTP_METHOD.DELETE || req.method === HTTP_METHOD.PUT) &&
-        userRole !== ROLE_NAME.ADMIN &&
-        userRole !== ROLE_NAME.SUPERADMIN
-      ) {
-        return res.status(400).send({
-          message: `You are not allowed to update or delete this ${resource}.`,
-        });
-      }
-    }
     if (
       req.method === HTTP_METHOD.DELETE &&
       (userRole === ROLE_NAME.ADMIN || userRole === ROLE_NAME.SUPERADMIN)
@@ -40,8 +23,20 @@ const checkIfUserAllowed = (resource) => async (req, res, next) => {
     let currentResource;
     switch (resource) {
       case RESOURCE.USER:
-        currentResource = await User.query().findById(id);
-        break;
+        if (
+          req.method === HTTP_METHOD.PUT &&
+          (userRole === ROLE_NAME.ADMIN || userRole === ROLE_NAME.SUPERADMIN)
+        ) {
+          next();
+          return;
+        } else if (
+          req.method === HTTP_METHOD.PUT &&
+          userRole !== ROLE_NAME.ADMIN &&
+          userRole !== ROLE_NAME.SUPERADMIN
+        ) {
+          currentResource = await User.query().findById(id);
+          break;
+        }
       case RESOURCE.USERPROFILE:
         currentResource = await UserProfile.query().findById(id);
         break;
@@ -57,7 +52,7 @@ const checkIfUserAllowed = (resource) => async (req, res, next) => {
 
     if (!currentResource) {
       return res.status(404).send({
-        message: `User or Post with id ${id} not found!`,
+        message: `${resource} with id ${id} not found!`,
       });
     }
 

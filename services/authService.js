056b8a6c32b3ba60cadfe369ee72d861969bd21f
Daniel <danielhakobyan460@gmail.com>
2024-05-25 20:52:45 +0400

    blocked access to routes if user is not active

--- services/authService.js
+++ services/authService.js
@@ -20,6 +20,12 @@ const signin = async (email, password, username) => {
 
     const user = await userQuery.first();
 
+    if (user.is_active === false) {
+      throw new Error(
+        "User is not active. You did'nt verify your email. If you did, please contact support."
+      );
+    }
+
     const hashedPassword = user.password;
 
     const isSame = await bcrypt.compare(password, hashedPassword);
@@ -89,7 +95,7 @@ const resetPassword = async (
     return sendSucces;
   } catch (err) {
     console.error(err);
-    throw new Error(err)
+    throw new Error(err);
   }
 };
 
